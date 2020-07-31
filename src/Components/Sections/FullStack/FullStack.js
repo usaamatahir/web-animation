f324ee9cfb96fd0fd7e63eb72157b2ca21a30e1d
Usama Tahir <usamatahir8330@gmail.com>
2020-07-31 10:45:26 +0500

    css changes

--- src/Components/Sections/FullStack/FullStack.js
+++ src/Components/Sections/FullStack/FullStack.js
@@ -17,7 +17,7 @@ const fullStack = () => {
                 <span></span>
                 <span></span>
                 <div className={styles.content}>
-                    <h2>react web developer</h2>
+                    <h2>full stack developer</h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
             </div>
--- src/Components/Sections/MobileApp/MobileApp.js
+++ src/Components/Sections/MobileApp/MobileApp.js
@@ -5,7 +5,7 @@ import styles from './MobileApp.module.css';
 const MobileApp = () => {
     return (
         <div className={styles.vision}>
-            <h1>android and iphone developer</h1>
+            <h1>android and ios app developer</h1>
             <div data-aos="slide-left" className={styles.svgImage}>
                 <img src={require('./android.svg')} alt="SVG logo" />
             </div>
@@ -14,7 +14,7 @@ const MobileApp = () => {
                 <span></span>
                 <span></span>
                 <div className={styles.content}>
-                    <h2>react web developer</h2>
+                    <h2>mobile app developer</h2>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
             </div>
